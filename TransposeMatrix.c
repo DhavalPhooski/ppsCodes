8d243cd95f147fd8921b596291fd52ed2885ca06
DhavalPhooski <dhaval.patel.290405@gmail.com>
2024-04-07 03:57:23 +0530

    TransposeMatrix

---
+++ TransposeMatrix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int n,m,i,j,k;
+    printf("Enter the value of 'n' of your nXm matrix : ");
+    scanf("%d",&n);
+    printf("Enter the value of 'm' of your nXm matrix : ");
+    scanf("%d",&m);
+    int matrix[n][m];
+    int transpose[m][n];
+
+    //scanning matrix
+    printf("Enter the values of your matrix's Elements :\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    //transpose logic 
+    for(i=0;i<n;i++){
+        for(j=0;  j<m;  j++){
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    //printing matrix
+    printf("Transpose of this is : \n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",transpose[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
